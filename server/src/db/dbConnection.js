b1eb87a3a339b9c7a14dd06c47e6ed4856e0c272
FabianCristancho <fabiancrisrincon@gmail.com>
2020-08-10 12:02:12 -0500

    db

--- server/src/db/dbConnection.js
+++ server/src/db/dbConnection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: process.env.DATABASE_HOST || '127.0.0.1',
+    host: '192.168.100.36',
     //host: 'db', //Para container
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'admin123',
     // password: 'admin123', //Para container
     database: 'covid_reports'
   });
