943c3ec24d10d9c57f787edb24ee91de6a23bc1b
NicolasMorenoPuerto <fredy.moreno01@uptc.edu.co>
2020-08-10 16:35:29 -0500

    Piechart en client.js

--- client/js/client.js
+++ client/js/client.js
@@ -1,20 +1,42 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      time: 'hola',
-      startTime: 0,
-      message: 'Funciona'
-    },
+  el: "#app",
+  data: {
+    time: "hola",
+    startTime: 0,
+    message: "Funciona",
+  },
 
-    methods: {
-        getTime: function () {
-            console.log('dfsd');
-            axios
-            .get('http://localhost:3000/item')
-            .then(response => {
-                this.message = response.data.age;
-            }
-            );
-        }
-    }
+  methods: {
+    getTime: function () {
+      console.log("dfsd");
+      axios.get("http://localhost:3000/item").then((response) => {
+        this.message = response.data.age;
+      });
+    },
+    drawStart: function () {
+      google.charts.load("current", { packages: ["corechart"] });
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ["Task", "Hours per Day"],
+          ["Work", 11],
+          ["Eat", 2],
+          ["Commute", 2],
+          ["Watch TV", 8],
+          ["Sleep", 7],
+        ]);
+
+        var options = {
+          title: "My Daily Activities",
+        };
+
+        var chart = new google.visualization.PieChart(
+          document.getElementById("piechart")
+        );
+
+        chart.draw(data, options);
+      }
+    },
+  },
 });
