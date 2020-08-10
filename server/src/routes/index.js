505ff684d4b23b2cc0c236e7c9c51bcde83b517d
NicolasMorenoPuerto <fredy.moreno01@uptc.edu.co>
2020-08-10 09:52:35 -0500

    Retorno de la pagina de inicio

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const dbConnection = require('../db/dbConnection');
 const connection = dbConnection();
 const router = express.Router();
-
+const path = require('path');
 
 router.get('/', (req, res) => res.send('Hello World!'));
 
 router.post('/data', (req, res) => {
      console.log("Entra");
      console.log(req.body);
+     res.sendFile(path.join(__dirname, '../../../client', 'index.html'));
 });
+
+router.get('/path', (req, res) => res.sendFile(path.join(__dirname+'../../../client/index.html')));
 
 module.exports = router;
