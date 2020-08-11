848f40ff0b95e01d43f47dc26e7b5acce138c824
FabianCristancho <fabiancrisrincon@gmail.com>
2020-08-11 09:45:54 -0500

    Config db

--- server/src/db/dbConnection.js
+++ server/src/db/dbConnection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    //host: 'db', //Para container
+    host: '192.168.100.36',
+    //host: 'db', process.env.DATABASE_HOST || '127.0.0.1'//Para container
     port: 3306,
     user: 'root',
     password: 'admin123',
